a8d8dcbbed0c9c3b7dc0cc0f576b5bcccbef9d1a
Vladimir <127045411+Vladimir96Ru@users.noreply.github.com>
2023-03-11 21:24:38 +0500

    Add files via upload

---
+++ 2.EPS/EPS/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  EPS Lab №1-2-12
+//
+//  Created by Vladimir S. on 26.06.14.
+//  Copyright (c) 2014 Vladimir S. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    float eps = 0.001;
+    float ykp = 0.0;
+    for( unsigned k = 1; k < 100; k++ )
+    {
+        float yk = ( ykp+1 )/( ykp+2 );
+        if( yk-ykp < eps )
+        {
+            std::cout << "Номер члена: " << k << std::endl;
+            std::cout << "Значение члена: " << yk << std::endl;
+            return 0;
+        }
+        ykp = yk;
+    }
+    std::cout << "Член не найден." << std::endl;
+    return 0;
+}
+
